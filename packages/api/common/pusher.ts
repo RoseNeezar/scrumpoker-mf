becb8d880f96207a230e149555687f31a89b9a48
neezar <rforlyffe@gmail.com>
2023-02-04 18:38:39 +0800

    turborepo envs are retarded

--- packages/api/common/pusher.ts
+++ packages/api/common/pusher.ts
@@ -1,12 +1,11 @@
 import PusherServer from "pusher";
-import { env } from "@scrumpoker-mf/env";
 
 export const pusherServerClient = new PusherServer({
-  appId: process.env.PUSHER_APP_ID!,
-  key: process.env.REACT_APP_PUSHER_APP_KEY!,
-  secret: process.env.REACT_APP_PUSHER_APP_SECRET!,
-  host: process.env.REACT_APP_PUSHER_SERVER_HOST!,
-  port: process.env.REACT_APP_PUSHER_SERVER_PORT!,
-  useTLS: process.env.REACT_APP_PUSHER_SERVER_TLS === "true",
-  cluster: process.env.REACT_APP_PUSHER_APP_CLUSTER!,
+  appId: "default",
+  key: "app-key",
+  secret: "app-secret",
+  host: "localhost",
+  port: "6001",
+  useTLS: false,
+  cluster: undefined,
 });
