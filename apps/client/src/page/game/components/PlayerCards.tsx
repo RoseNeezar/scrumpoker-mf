f7318a37a6d30b18ebfa2f314b22507cf58535b6
neezar <rforlyffe@gmail.com>
2023-02-05 21:45:09 +0800

    fix: button loading

--- apps/client/src/page/game/components/PlayerCards.tsx
+++ apps/client/src/page/game/components/PlayerCards.tsx
@@ -1,29 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { useCurrentPlayer, useGame } from "../../../store/useGame";
 import { trpc } from "../../../utils/trpc";
 
 type Props = {};
 
 const PlayerCards = (props: Props) => {
+  const [loadingIndex, setLoadingIndex] = useState(-1);
   const data = useGame();
   const currentPlayer = useCurrentPlayer();
-  const { mutateAsync, isLoading } = trpc.game.playerVote.useMutation();
+  const { mutateAsync, isLoading } = trpc.game.playerVote.useMutation({
+    onSuccess: () => {
+      setLoadingIndex(-1);
+    },
+  });
+
   return (
     <div className="m-auto grid grid-cols-2 gap-10 sm:grid-cols-3 md:grid-cols-4">
       {data.points.map((s, i) => {
         return (
           <button
             key={i}
-            onClick={() =>
+            onClick={() => {
+              setLoadingIndex(i);
               mutateAsync({
                 gameID: data.id,
                 playerId: currentPlayer!.id,
                 vote: s,
-              })
-            }
+              });
+            }}
             className="btn-secondary btn m-2 flex h-24 w-24 cursor-pointer items-center justify-center rounded-xl"
           >
-            {isLoading ? (
+            {isLoading && loadingIndex === i ? (
               <span className="loading btn border-none bg-transparent" />
             ) : (
               <h1 className="text-2xl font-bold ">{s}</h1>
