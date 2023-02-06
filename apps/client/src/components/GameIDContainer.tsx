9f10cb0d3be3783e396ad7f87615bd48644f45aa
neezar <rforlyffe@gmail.com>
2023-02-06 18:26:47 +0800

    fix: paths

--- apps/client/src/components/GameIDContainer.tsx
+++ apps/client/src/components/GameIDContainer.tsx
@@ -9,7 +9,7 @@ const copyUrlToClipboard = (path: string) => () => {
 const GameIDContainer: FC<{ id: string }> = ({ id }) => {
   return (
     <div
-      onClick={copyUrlToClipboard(`/game/${id}`)}
+      onClick={copyUrlToClipboard(`game/${id}`)}
       className="flex w-fit cursor-pointer items-center justify-center rounded-lg border-2 border-gray-800 bg-gray-600 p-3"
     >
       <div className="mr-3">Game ID: </div>
